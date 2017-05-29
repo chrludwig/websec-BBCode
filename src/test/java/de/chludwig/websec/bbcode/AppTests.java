aa2dd779525f083dd0589a36c94578bad8282c4b
Christoph Ludwig <christoph@chludwig.de>
2017-05-29 21:26:50 +0200

    Upgrade Spring Boot 1.5.3, kefirBB 1.5; switch to jar w/ embedded Tomcat

--- src/test/java/de/chludwig/websec/bbcode/AppTests.java
+++ src/test/java/de/chludwig/websec/bbcode/AppTests.java
@@ -2,31 +2,30 @@ package de.chludwig.websec.bbcode;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.boot.test.WebIntegrationTest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(BbCodeApplication.class)
-@WebIntegrationTest(randomPort = true)
-@DirtiesContext
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AppTests {
 
-    @Value("${local.server.port}")
-    private int port;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
     @Test
     public void testJspWithEl() throws Exception {
-        ResponseEntity<String> entity = new TestRestTemplate()
-                .getForEntity("http://localhost:" + this.port, String.class);
+        ResponseEntity<String> entity = testRestTemplate
+                .getForEntity("/", String.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         assertTrue("Wrong body:\n" + entity.getBody(),
                    entity.getBody().contains("<title>BBCode Test Form</title>"));
